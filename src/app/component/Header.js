e43abe3441813f6405dbc00db8f672b2eed8353b
Roshan panda <roshanpanda666@gmail.com>
2023-06-22 15:56:47 +0530

    commiting

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -8,14 +8,14 @@ const Header = () => {
     close(!open);
   }
   return (
-    <div className="flex gap-x-[60vw] justify-center lg:mt-10 mt-5 mb-5 h-7">
+    <div className="flex gap-x-[60vw] justify-center lg:mt-10 mt-5 mb-5 h-7 text-[1.6rem]">
       <div>
         <Link href='/'>
-        logo
+        <img src="movie-icon.png" alt="next" className="lg:h-[10vh] h-[5vh] w-[12vw] lg:w-[5vw]"/>
         </Link>
       </div>
       <div onClick={openfun} className="lg:hidden block">
-        =
+        <img src="menu-white.png" alt="" className="h-[5vh]"/>
       </div>
       <motion.div
         className={`flex justify-center items-center absolute h-[50%] bg-[rgb(9,158,158)]  w-[80%] lg:hidden
--- src/app/component/Moviecard.js
+++ src/app/component/Moviecard.js
@@ -7,7 +7,7 @@ const Moviecard = (curElem) => {
         <div className="flex justify-center items-center">
 
         <Link href={`/movie/${id}`}>
-            <div className="border-2 border-[rgb(9,158,158)] lg:h-72 h-72 w-60 lg:w-60 m-5 flex flex-col justify-center items-center hover:shadow-[0px_0px_40px_6px_rgb(9,158,158)] hover:h-80 hover:w-64 duration-500">
+            <div className="border-2 border-[rgb(9,158,158)] lg:h-80 h-80 w-80 lg:w-80 m-5 flex flex-col justify-center items-center hover:shadow-[0px_0px_40px_6px_rgb(9,158,158)] hover:h-[22rem] hover:w-[22rem] duration-500 p-4 rounded-md">
                 <div>
                     <img src={curElem.jawSummary.backgroundImage.url} alt="{title}" />
                 </div>
